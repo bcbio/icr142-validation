0edba432d4e1d581ff3f9818b56470d768901e0a
chapmanb <chapmanb@50mail.com>
2016-05-08 05:44:18 -0400

    Add Platypus to ICR validation results

--- scripts/bcbio_validation_plot.py
+++ scripts/bcbio_validation_plot.py
@@ -1,5 +1,5 @@
 import sys
 from bcbio.variation import validateplot
 
-title="ICR142 initial validation: GATK HaplotypeCaller, FreeBayes, VarDict"
+title="ICR142: GATK HaplotypeCaller, FreeBayes, VarDict, Platypus"
 validateplot.classifyplot_from_valfile(sys.argv[1], outtype="png", title=title)
